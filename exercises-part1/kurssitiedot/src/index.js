97d3c2762d15bea21229f00da9eeede50abdce1f
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-04-22 21:29:45 +0300

    Exercises part1 - kurssitiedot teht. 1a

---
+++ exercises-part1/kurssitiedot/src/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Header = (props) => {
+    return (
+      <>
+        <h1>{props.course}</h1>
+      </>
+    )
+}
+const Part = (props) => {
+    return (
+      <>
+        <p>{props.part} {props.exercise}</p>
+      </>
+   )
+} 
+
+const Content = (props) => { 
+    return (
+      <>
+        <Part part = {props.part1} exercise = {props.ex1} />
+        <Part part = {props.part2} exercise = {props.ex2} />
+        <Part part = {props.part3} exercise = {props.ex3} />
+      </>
+    )
+} 
+
+
+const Total = (props) => {
+    return (
+      <>
+        <p>yhteensä {props.total} tehtävää</p>
+      </>
+    )
+}
+
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys'
+  const part1 = 'Reactin perusteet'
+  const exercises1 = 10
+  const part2 = 'Tiedonvälitys propseilla'
+  const exercises2 = 7
+  const part3 = 'Komponenttien tila'
+  const exercises3 = 14
+
+  return (
+    <div>
+        <Header course={course} />
+        <Content part1={part1} ex1={exercises1} part2={part2} ex2={exercises2} part3={part3} ex3={exercises3} />
+        <Total total = {exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
