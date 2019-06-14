450445a5f8ee68a550d0f9a106cdaf1b68f079a1
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-06-14 22:44:53 +0300

    2.2: kurssitiedot step7

--- exercises-part2/kurssitiedot/src/components/Content.js
+++ exercises-part2/kurssitiedot/src/components/Content.js
@@ -10,11 +10,20 @@ const Content = ({content}) => {
         exercise = {part.exercises}
       />
     )
-    
+    const calculateTotal= () => {
+      const totalExercises = content.map(part => part.exercises)
+      let sum = 0;
+      for( let i=0; i < totalExercises.length; i++){
+        sum = sum + totalExercises[i]
+        console.log(sum)
+      }
+      return sum  
+    }  
   
     return (
       <div>
        {parts()}
+       <p><b>Total of {calculateTotal()} exercises</b></p>
         
       </div>
     )
